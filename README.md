ebc342d75237e3595a25b60dd3d180c53ecc7686
ianRickmers <ian.rickmers@usach.cl>
2023-06-10 11:20:38 -0400

    Documentación final

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# tarea-5-frontend
+# Tarea número 5 
+Para el funcionamiento, se debe ingresar a la página desde index.html y revisar la consola.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,9 @@
+//Autor: Ian Rickmers Blamey
 const notas= [6,8,9,2,5,10]
 
+//Funcion que recibe un arreglo de número y devuelve la suma de los números
+//Entrada: arreglo de números
+//Salida: suma de los números
 function sumaNotas(notas){
     let suma=0
     for(let i=0;i<notas.length;i++){
@@ -8,6 +12,9 @@ function sumaNotas(notas){
     return suma
 }
 
+//Funcion que recibe un arreglo de número y devuelve el promedio de los números
+//Entrada: arreglo de números
+//Salida: promedio de los números
 function promedio(notas){
     let promedio=0
     promedio=sumaNotas(notas)/notas.length
